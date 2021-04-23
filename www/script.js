0016efcf3b31ef5b52676ecd1d7d1ea7d1910209
Gustavo Gomes <gustsgomesbr@hotmail.com>
2021-04-23 19:50:39 -0400

    logica completa, footer criado

--- www/script.js
+++ www/script.js
@@ -1,5 +1,11 @@
 var tabelas;
 var valorSolicitado;
+var totalValue = [];
+
+var clientSolicitation = {
+  tabela: "",
+  parcelas: ""
+}
 function Calcular(value){ 
   if(value <= 299.99 || value >= 10000.1){ //evitando valores abaixo de 300 e acima de 10.000 mesmo o back-end tendo essa defesa.
     alert('error: Valor não autorizado!')
@@ -67,6 +73,7 @@ function CreateTables(){
     tr.appendChild(th5);
     tr.appendChild(th6);
     for (let f = 0; f < tabelas[i].installments.length; f++) {
+      //criando os elementos td e o tr
       var tableTr = document.createElement('tr');
       var tableTd1 = document.createElement('td');
       var tableTd2 = document.createElement('td');
@@ -75,7 +82,11 @@ function CreateTables(){
       var tableTd5 = document.createElement('td');
       var tableTd6 = document.createElement('td');
 
+      tableTr.id = tabelas[i].installments[f].id;
+      tableTr.setAttribute('tabela', i);
+      tableTr.className = "clickableTr";
       tableTd1.innerHTML = tabelas[i].installments[f].id;
+
       tableTd2.innerHTML = tabelas[i].installments[f].installmentInterest * 100;
       tableTd2.innerHTML = tableTd2.innerHTML + '%';
 
@@ -89,7 +100,8 @@ function CreateTables(){
       tableTd4.innerHTML = valorSolicitado * tabelas[i].installments[f].installmentInterest;
       tableTd4.innerHTML = parseFloat(tableTd4.innerHTML) + valorSolicitado;
       tableTd4.innerHTML = "R$" + parseFloat(tableTd4.innerHTML).toFixed(2);
-
+      totalValue.push([])
+      totalValue[i].push(tableTd4.innerHTML)
       //porcentagem de comissão
       tableTd5.innerHTML = tabelas[i].installments[f].comission * 100;
       tableTd5.innerHTML = tableTd5.innerHTML + '%';
@@ -98,7 +110,7 @@ function CreateTables(){
       tableTd6.innerHTML = (valorSolicitado * tabelas[i].installments[f].installmentInterest) * tabelas[i].installments[f].comission;
       tableTd6.innerHTML = "R$" + parseFloat(tableTd6.innerHTML).toFixed(2);
 
-
+      //colocando todos os TDs dentro do Tr e o tr dentro da tabela index;
       tabela.appendChild(tableTr);
       tableTr.appendChild(tableTd1);
       tableTr.appendChild(tableTd2);
@@ -107,14 +119,40 @@ function CreateTables(){
       tableTr.appendChild(tableTd5);
       tableTr.appendChild(tableTd6);
 
-
     }
+  }
+  let clickableRow = document.getElementsByClassName('clickableTr');
+  for (let g = 0; g < clickableRow.length; g++) {
+    clickableRow[g].addEventListener("click", function (){
+      var numTabela = clickableRow[g].getAttribute('tabela');
+      clientSolicitation.tabela = numTabela;
+      clientSolicitation.parcelas = clickableRow[g].id;
+      CreateFooter(tabelas[numTabela].name, clickableRow[g].id, totalValue[numTabela][clickableRow[g].id-1])
+    })
   }
 }
 
-  function FormatToTable(i, f){
-    
+function CreateFooter(table, parcelas, valorParcela){
+  var footer = document.createElement('footer');
+  var nomeh1 = document.createElement('h1');
+  var parcelah1 = document.createElement('h1');
+  var valorParcelah1 = document.createElement('h1');
 
+  nomeh1.innerHTML = `Nome: ${table}`
+  parcelah1.innerHTML = `Parcelas: ${parcelas}`
+  valorParcelah1.innerHTML = `Valor da Parcela: ${valorParcela}`
+  var submitBtn = document.createElement('input');
+  submitBtn.type = "Submit";
+  submitBtn.value = "Avançar";
 
+  document.body.appendChild(footer);
+  footer.appendChild(nomeh1);
+  footer.appendChild(parcelah1);
+  footer.appendChild(valorParcelah1);
+  footer.appendChild(submitBtn);
 
-  }
+  submitBtn.addEventListener("click", function(){
+    window.open(`?tab=${clientSolicitation.tabela}&parc=${clientSolicitation.parcelas}`, '_self')
+  })
+
+}
