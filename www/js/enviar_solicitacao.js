4d285b4f944ebd5a43af364f5d01d1b675ab9281
Gustavo Gomes <gustsgomesbr@hotmail.com>
2021-04-24 19:06:11 -0400

    :tada: Enviando solicitação completa do usuario para o back-end

---
+++ www/js/enviar_solicitacao.js
@@ -0,0 +1,59 @@
+var userSolicitation = {}
+var url_string =window.location.href
+var url = new URL(url_string);
+var tab = url.searchParams.get("tab");
+var parc = url.searchParams.get("parc");
+var valor = url.searchParams.get("valor");
+var cpf = url.searchParams.get('cpf');
+
+userSolicitation.tabela = tab;
+userSolicitation.parcelas = parc;
+userSolicitation.valor = valor;
+userSolicitation.cpf = cpf;
+userSolicitation.cardInfo = {}
+
+function CheckCard(){
+  var inputNome = document.getElementById('nomeCartao');
+  var inputCartao = document.getElementById('numeroCartao');
+  var inputValidade = document.getElementById('validadeCartao');
+  var inputCodigo = document.getElementById('codigoCartao');
+
+  if(inputNome.value === "" || inputCartao.value === "" || inputValidade.value === "" || inputCodigo.value === "" || inputCartao.value.length <= 15 || inputCodigo.value.length <= 2 || inputNome.value.length <= 5 || inputValidade.value.length <=4){
+    alert('Por Favor, preencha todos os dados do cartão corretamente!');
+  }else{
+    userSolicitation.cardInfo.nome = inputNome.value;
+    userSolicitation.cardInfo.numero = inputCartao.value;
+    userSolicitation.cardInfo.validade = inputValidade.value;
+    userSolicitation.cardInfo.codigo = inputCodigo.value;
+    console.log(userSolicitation)
+    PostData();
+  }
+
+
+}
+
+
+function PostData(){
+    async function sendData(){
+      try{
+        const response = await fetch(`http://localhost:3000/emprestimos/solicitar`, {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(userSolicitation)
+        });
+        const data = await response.json()
+        if(data.resposta === "ok"){
+          console.log('Dados enviados com sucesso!');
+
+        }
+
+      }
+      catch (error){
+        console.error(error);
+      }
+    }
+    sendData();    
+}
